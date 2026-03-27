23cd6d2449fc574c773c57af03d20a51a0950341
Ayush-Debnath <debnathayush48@gmail.com>
2026-03-27 22:20:18 +0530

    data transfromation done

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 
+from re import S
+
 from mlOpsProject import logger
 from mlOpsProject.components import data_ingestion
 from mlOpsProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from mlOpsProject.pipeline.stage_02_data_validation import DataValidationPipeline
+from mlOpsProject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 
 STAGE_NAME="Data Ingestion stage"
 
@@ -21,6 +24,19 @@ try:
     logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
     data_validation=DataValidationPipeline()
     data_validation.main()
+    logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+
+STAGE_NAME="Data Transformation stage"
+
+try:
+    logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
+    obj=DataTransformationTrainingPipeline()
+    obj.main()
     logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<\n\nx==========x")
 except Exception as e:
     logger.exception(e)
--- src/mlOpsProject/config/configuration.py
+++ src/mlOpsProject/config/configuration.py
@@ -1,6 +1,7 @@
 from mlOpsProject.constants import *
+from pathlib import Path
 from mlOpsProject.utils.common import read_yaml,create_directories
-from mlOpsProject.entity.config_entity import DataIngestionConfig, DataValidationConfig
+from mlOpsProject.entity.config_entity import DataIngestionConfig, DataTransformationConfig, DataValidationConfig
 
 class ConfigurationManager:
     def __init__(
@@ -43,4 +44,17 @@ class ConfigurationManager:
         )
         
         return data_validation_config
+    
+
+    def get_data_transformation_config(self) -> DataTransformationConfig:
+        config = self.config.data_transformation
+
+        create_directories([config.root_dir])
+
+        data_transformation_config=DataTransformationConfig(
+            root_dir=Path(config.root_dir),
+            data_path=Path(config.data_path)
+        )
+
+        return data_transformation_config
     
